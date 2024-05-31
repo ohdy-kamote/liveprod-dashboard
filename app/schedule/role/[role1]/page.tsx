3f8e1316c0deaaef085260223da37bcc4c889ea8
phil002109 <vermontphil.paguiligan@gmail.com>
2024-05-31 19:02:55 +0800

    Redesign arrow button

--- app/schedule/role/[role1]/page.tsx
+++ app/schedule/role/[role1]/page.tsx
@@ -16,8 +16,12 @@ export default async function FirstRoleDisplay({ params }: {params: { role1: str
       <div className="relative">
         <SchedulesByRole role={decodeURI(params.role1)} />
         <div className="flex gap-4 w-full justify-end absolute -bottom-9 pr-3">
-          <Link href={`/schedule/role/${roledetails?.prev?.data}`}><BsArrowLeftCircle size={27} /></Link>
-          <Link href={`/schedule/role/${roledetails?.next?.data}`}><BsArrowRightCircle size={27} /></Link>
+          <Link className="hover:bg-slate-800 hover:text-slate-50 rounded-full" href={`/schedule/role/${roledetails?.prev?.data}`}>
+            <BsArrowLeftCircle size={27} />
+          </Link>
+          <Link className="hover:bg-slate-800 hover:text-slate-50 rounded-full" href={`/schedule/role/${roledetails?.next?.data}`}>
+            <BsArrowRightCircle size={27} />
+          </Link>
         </div>
       </div>
     </Fragment>
--- app/schedule/segment/[segment]/page.tsx
+++ app/schedule/segment/[segment]/page.tsx
@@ -1,5 +1,6 @@
 import CpScheduleBySegment from "@/components/ScheduleBySegment";
 import Link from "next/link";
+import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 
 export default async function ScheduleBySegment({searchParams}: {searchParams: {increment: string}}) {
   const increment = parseInt(searchParams?.increment || "0");
@@ -9,8 +10,12 @@ export default async function ScheduleBySegment({searchParams}: {searchParams: {
       <div className="flex flex-col w-5/6 gap-3">
         <CpScheduleBySegment increment={increment} />
         <div className="flex gap-3 w-full justify-end">
-          <Link href={`/schedule/segment/audio?increment=${increment-1}`}>{"<< Prev"}</Link>
-          <Link href={`/schedule/segment/audio?increment=${increment+1}`}>{"Next >>"}</Link>
+          <Link className="hover:bg-slate-800 hover:text-slate-50 rounded-full" href={`/schedule/segment/audio?increment=${increment-1}`}>
+            <BsArrowLeftCircle size={27} />
+          </Link>
+          <Link className="hover:bg-slate-800 hover:text-slate-50 rounded-full" href={`/schedule/segment/audio?increment=${increment+1}`}>
+            <BsArrowRightCircle size={27} />
+          </Link>
         </div>
       </div>
     </div>
