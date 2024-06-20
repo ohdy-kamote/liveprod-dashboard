9bb26dcf49663d749e2ed6fa644a2d6ae7ff692d
phil002109 <vermontphil.paguiligan@gmail.com>
2024-06-20 20:06:30 +0800

    Sched by segment allow split view, up to 2 weeks

---
+++ components/client/CCScheduleBySegment.tsx
@@ -0,0 +1,68 @@
+import SCVolunteerCell from "@/components/server/SCVolunteerCell";
+import { category, common } from "@/utils/constants";
+import { formatDate } from "@/utils/helpers";
+
+interface Schedule {
+  from: string
+  to: string
+  data: any
+}
+
+export default async function CCScheduleBySegment({ schedule }: { schedule: Schedule }) {
+  const convertData = (data: any) => {
+    const convertedData: any = {};
+    data.forEach((item: any) => {
+      convertedData[item._id] = {};
+      item.service.forEach((service: any) => {
+        convertedData[item._id][service.role] = service;
+      });
+    });
+    return convertedData;
+  };
+
+  const convertedData: any = convertData(schedule.data);
+
+  return (
+    <div className="flex items-center justify-center w-full">
+      <table className="table-auto border border-slate-500 w-full bg-slate-300">
+        <thead>
+          <tr>
+            <th className="border-l border-slate-500 bg-slate-100"></th>
+            <th className="bg-slate-100"></th>
+            <th className="border-l border-slate-500">{formatDate(schedule.from)}</th>
+            <th className="border-l border-slate-500 bg-green-400">{formatDate(schedule.to)}</th>
+            <th className="border-l border-slate-500 bg-green-400">{formatDate(schedule.to)}</th>
+            <th className="border-l border-slate-500 bg-green-400">{formatDate(schedule.to)}</th>
+          </tr>
+          <tr>
+            <th className="border-l border-slate-500 bg-slate-100"></th>
+            <th className="bg-slate-100"></th>
+            <th className="border-l border-slate-500 uppercase">{category.SATURDAY_SERVICE}</th>
+            <th className="border-l border-slate-500 bg-green-400 uppercase">{common.FIRST_SERVICE}</th>
+            <th className="border-l border-slate-500 bg-green-400 uppercase">{common.SECOND_SERVICE}</th>
+            <th className="border-l border-slate-500 bg-green-400 uppercase">{common.THIRD_SERVICE}</th>
+          </tr>
+        </thead>
+        <tbody>
+          { category.ROLES.map((role, i) => {
+            const sns = convertedData?.[category.SATURDAY_SERVICE]?.[role]
+            const firstService = convertedData?.[common.FIRST_SERVICE]?.[role]
+            const secondService = convertedData?.[common.SECOND_SERVICE]?.[role]
+            const thirdService = convertedData?.[common.THIRD_SERVICE]?.[role]
+
+            return (
+              <tr key={i}>
+                <td className="border border-slate-500 bg-slate-100 w-9 text-center h-8">{i+1}</td>
+                <td className="border border-slate-500 bg-slate-100 px-1 w-64 uppercase">{role}</td>
+                <SCVolunteerCell service={sns} />
+                <SCVolunteerCell service={firstService} />
+                <SCVolunteerCell service={secondService} />
+                <SCVolunteerCell service={thirdService} />
+              </tr>
+            )})
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
--- components/server/SCVolunteerCell.tsx
+++ components/server/SCVolunteerCell.tsx
@@ -1,8 +1,8 @@
 import { checkAuth } from "@/utils/helpers";
-import VolunteerCell from "@/components/client/CCVolunteerCell";
+import CCVolunteerCell from "@/components/client/CCVolunteerCell";
 
 export default async function SCVolunteerCell(props: { service: any }) {
   const isAuthenticated = await checkAuth();
 
-  return <VolunteerCell service={props.service} isAuthenticated={isAuthenticated} />;
+  return <CCVolunteerCell service={props.service} isAuthenticated={isAuthenticated} />;
 }
