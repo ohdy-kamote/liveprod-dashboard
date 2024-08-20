d2730a9355a1bd08a6af5c7e0f90d4a36b762155
phil002109 <vermontphil.paguiligan@gmail.com>
2024-08-20 18:59:38 +0800

    Disable cells for unsupported roles

--- components/client/CCVolunteerCell.tsx
+++ components/client/CCVolunteerCell.tsx
@@ -6,12 +6,12 @@ export default function CCVolunteerCell({ service, isAuthenticated }: { service:
   const router = useRouter();
 
   const handleClick = () => {
-    if (!isAuthenticated) return;
+    if (!isAuthenticated || !service?.id) return;
     router.push(`/schedule/assign-volunteer/${service?.id}`);
   };
 
   return (
-    <td onClick={handleClick} className={`${isAuthenticated ? "hover:bg-slate-200 cursor-pointer" : ""} border border-slate-500 bg-slate-100 min-w-20`}>
+    <td onClick={handleClick} className={`${isAuthenticated && !!service?.id ? "hover:bg-slate-200 cursor-pointer" : ""} ${!!service?.id ? "bg-slate-100": "bg-gray-300"} border border-slate-500 min-w-20`}>
       <div className="overflow-hidden h-6">
         {service?.volunteer?.[0]?.firstName}
       </div>
