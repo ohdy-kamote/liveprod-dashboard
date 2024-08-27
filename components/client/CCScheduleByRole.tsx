c9743b16b9848b103ca336db9a82bd1024cde43c
phil002109 <vermontphil.paguiligan@gmail.com>
2024-08-27 18:29:56 +0800

    Added bottom border to Schedules table

--- components/client/CCScheduleByRole.tsx
+++ components/client/CCScheduleByRole.tsx
@@ -10,8 +10,8 @@ export default function CCSchedulesByRole({ role, service, children }: { role: s
       <div className="bg-slate-900 border border-slate-900 flex justify-center">
         <RoleDropdown role={role} />
       </div>
-      <div className="relative h-[500px] overflow-y-scroll no-scrollbar snap-y snap-mandatory transition-all delay-1000">
-        <table className="table-auto border w-full text-sm">
+      <div className="relative h-[500px] overflow-y-scroll no-scrollbar snap-y snap-mandatory transition-all delay-1000 border-b border-b-slate-300">
+        <table className="table-auto w-full text-sm">
           <thead>
             <tr className="snap-start">
               <th className="sticky top-px bg-slate-300 border border-slate-300 w-20"></th>
