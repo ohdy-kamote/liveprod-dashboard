aa78efcf168e308ed45c7de6b5d3629991c07a48
phil002109 <vermontphil.paguiligan@gmail.com>
2024-08-27 18:20:13 +0800

    Custom border for VolunteerCell accdg to pathname

--- components/client/CCVolunteerCell.tsx
+++ components/client/CCVolunteerCell.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function CCVolunteerCell({ service, isAuthenticated }: { service: any, isAuthenticated: boolean }) {
   const router = useRouter();
+  const pathname = usePathname();
+
+  const darkBorder = pathname.includes("schedule/segment")
 
   const handleClick = () => {
     if (!isAuthenticated || !service?.id) return;
@@ -11,7 +14,7 @@ export default function CCVolunteerCell({ service, isAuthenticated }: { service:
   };
 
   return (
-    <td onClick={handleClick} className={`${isAuthenticated && !!service?.id ? "hover:bg-slate-300 cursor-pointer" : ""} ${!!service?.id ? "": "bg-gray-300"} border border-slate-300 min-w-20`}>
+    <td onClick={handleClick} className={`${isAuthenticated && !!service?.id ? "hover:bg-slate-300 cursor-pointer" : ""} ${!!service?.id ? "": "bg-gray-300"} border ${darkBorder ? "border-slate-500" : "border-slate-300"} min-w-20`}>
       <div className="overflow-hidden h-6">
         {service?.volunteer?.[0]?.firstName}
       </div>
