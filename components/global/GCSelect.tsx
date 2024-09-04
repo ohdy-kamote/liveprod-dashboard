2448e74ab1d4d85d0ffa354d786a686e4e96575a
phil002109 <vermontphil.paguiligan@gmail.com>
2024-09-04 18:40:32 +0800

    Add blank initial value on disabled

--- components/global/GCSelect.tsx
+++ components/global/GCSelect.tsx
@@ -20,7 +20,7 @@ export default function GCSelect({ label, value, options, disabled, uppercase = 
         onChange={onChange}
         className={`${uppercase ? "uppercase" : "capitalize"} bg-zinc-100 rounded-sm border border-b-slate-400 focus:border-b-sky-500 px-3 py-2 focus:outline-none border-r-8 border-r-transparent text-slate-700 disabled:opacity-100 disabled:appearance-none disabled:border-r-0`}
       >
-        {!value && <option className="text-center" key={0} value={undefined}>{`- Select to add -`}</option>}
+        {!value && <option className="text-center" key={0} value={undefined}>{disabled ? "" : "- Select to add -"}</option>}
         {options.map((val, i) => (
           <option className="" key={i+1} value={val}>{val}</option>
         ))}
