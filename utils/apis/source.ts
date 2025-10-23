95e38a5fade4576c37423f5d6dce113f369998c0
Oscar Dacome <ohdy@Oscars-MacBook-Air.local>
2025-10-23 21:05:36 +0800

    Fix URLs to use VERCEL_URL for any deployment

--- utils/apis/source.ts
+++ utils/apis/source.ts
@@ -1,3 +1,3 @@
-export const SOURCE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://ccf-liveprod.vercel.app'
+export const SOURCE_URL = process.env.VERCEL_URL 
+  ? `https://${process.env.VERCEL_URL}`
   : process.env.SOURCE_URL || process.env.NEXT_PUBLIC_SOURCE_URL || 'http://localhost:3000';
