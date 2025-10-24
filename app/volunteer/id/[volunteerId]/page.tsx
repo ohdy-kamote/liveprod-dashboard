b0e9666316f03ff02e22539ae29a73c580c6039c
Oscar Dacome <ohdy@Oscars-MacBook-Air.local>
2025-10-24 19:45:48 +0800

    Remove try-catch interfering with redirect

--- app/volunteer/id/[volunteerId]/page.tsx
+++ app/volunteer/id/[volunteerId]/page.tsx
@@ -3,20 +3,15 @@ import connectMongoDB from "@/libs/mongodb";
 import Volunteer from "@/models/volunteer";
 
 export default async function VolunteerByIdPage({ params }: { params: { volunteerId: string } }) {
-  try {
-    console.log('Looking up volunteer ID:', params.volunteerId);
-    await connectMongoDB();
-    const volunteer = await Volunteer.findOne({ volunteerId: params.volunteerId });
-    
-    if (!volunteer) {
-      console.log('Volunteer not found, redirecting to /volunteer/all');
-      redirect("/volunteer/all");
-    }
-    
-    console.log('Volunteer found, redirecting to profile:', volunteer._id);
-    redirect(`/volunteer/profile/${volunteer._id}`);
-  } catch (error) {
-    console.log('Error in volunteer lookup:', error);
+  console.log('Looking up volunteer ID:', params.volunteerId);
+  await connectMongoDB();
+  const volunteer = await Volunteer.findOne({ volunteerId: params.volunteerId });
+  
+  if (!volunteer) {
+    console.log('Volunteer not found, redirecting to /volunteer/all');
     redirect("/volunteer/all");
   }
+  
+  console.log('Volunteer found, redirecting to profile:', volunteer._id);
+  redirect(`/volunteer/profile/${volunteer._id}`);
 }
