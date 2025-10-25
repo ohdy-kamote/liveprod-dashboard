2b5dbe8561234d42f1a9c10025c1205df76bc331
Oscar Dacome <ohdy@Oscars-MacBook-Air.local>
2025-10-25 11:20:37 +0800

    Temporarily bypass authentication for testing volunteer assignment

--- components/server/SCVolunteerCell.tsx
+++ components/server/SCVolunteerCell.tsx
@@ -2,7 +2,8 @@ import { checkAuth } from "@/utils/helpersServer";
 import CCVolunteerCell from "@/components/client/CCVolunteerCell";
 
 export default async function SCVolunteerCell(props: { service: any }) {
-  const isAuthenticated = await checkAuth();
+  // Temporarily bypass authentication for testing
+  const isAuthenticated = true;
   console.log('SCVolunteerCell - isAuthenticated:', isAuthenticated);
 
   return <CCVolunteerCell service={props.service} isAuthenticated={isAuthenticated} />;
