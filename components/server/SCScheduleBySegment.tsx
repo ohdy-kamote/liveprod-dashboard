038bac9396c565470f2b324474684927106e7e23
Oscar Dacome <ohdy@Oscars-MacBook-Air.local>
2025-10-25 10:09:49 +0800

    Fix critical API calls with direct database queries

--- components/server/SCScheduleBySegment.tsx
+++ components/server/SCScheduleBySegment.tsx
@@ -1,4 +1,3 @@
-import { getSchduleByDateRange } from "@/utils/apis/get";
 import { getNextService } from "@/utils/helpers";
 import { auth } from "@/auth";
 import Link from "next/link";
@@ -6,18 +5,26 @@ import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 import CCScheduleBySegment from "../client/CCScheduleBySegment";
 import CCMergedScheduleBySegment from "../client/CCMergedScheduleBySegment";
 import GCTabLInk from '../global/tabs/GCTabLink';
+import connectMongoDB from "@/libs/mongodb";
+import Schedule from "@/models/schedule";
 
 export default async function SCScheduleBySegment({increment, service}: {increment: number, service: string}) {
   const session = await auth();
   const isAuthenticated = (session?.user as any)?.isAdmin;
   const serviceDate1 = getNextService(increment);
   const serviceDate2 = getNextService(increment+1);
-  const serviceDate3 = getNextService(increment+2);
-  const serviceDate4 = getNextService(increment+3);
-  const schedule1 = await getSchduleByDateRange(serviceDate1.saturday, serviceDate1.sunday);
-  const schedule2 = await getSchduleByDateRange(serviceDate2.saturday, serviceDate2.sunday);
-  const schedule3 = await getSchduleByDateRange(serviceDate3.saturday, serviceDate3.sunday);
-  const schedule4 = await getSchduleByDateRange(serviceDate4.saturday, serviceDate4.sunday);
+  
+  // Get schedules directly from database
+  await connectMongoDB();
+  const schedules1 = await Schedule.find({
+    date: { $gte: serviceDate1.saturday, $lte: serviceDate1.sunday }
+  });
+  const schedules2 = await Schedule.find({
+    date: { $gte: serviceDate2.saturday, $lte: serviceDate2.sunday }
+  });
+  
+  const schedule1 = { saturday: serviceDate1.saturday, sunday: serviceDate1.sunday, data: schedules1 };
+  const schedule2 = { saturday: serviceDate2.saturday, sunday: serviceDate2.sunday, data: schedules2 };
 
   return (
     <div className="flex justify-center">
