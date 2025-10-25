e2f762c5ed5989ca924622c0ee3e082cf5e60f8a
Oscar Dacome <ohdy@Oscars-MacBook-Air.local>
2025-10-25 10:59:19 +0800

    Revert authentication to normal after URL fix

--- components/server/SCVolunteerCell.tsx
+++ components/server/SCVolunteerCell.tsx
@@ -2,8 +2,7 @@ import { checkAuth } from "@/utils/helpersServer";
 import CCVolunteerCell from "@/components/client/CCVolunteerCell";
 
 export default async function SCVolunteerCell(props: { service: any }) {
-  // Temporarily hardcode authentication to true for testing
-  const isAuthenticated = true;
+  const isAuthenticated = await checkAuth();
   console.log('SCVolunteerCell - isAuthenticated:', isAuthenticated);
 
   return <CCVolunteerCell service={props.service} isAuthenticated={isAuthenticated} />;
