e58bbf49991b0d29cb47ec0bfdab11afb01b133c
Oscar Dacome <ohdy@Oscars-MacBook-Air.local>
2025-10-25 10:47:53 +0800

    Temporarily hardcode authentication to test cell unlock

--- components/server/SCVolunteerCell.tsx
+++ components/server/SCVolunteerCell.tsx
@@ -2,7 +2,8 @@ import { checkAuth } from "@/utils/helpersServer";
 import CCVolunteerCell from "@/components/client/CCVolunteerCell";
 
 export default async function SCVolunteerCell(props: { service: any }) {
-  const isAuthenticated = await checkAuth();
+  // Temporarily hardcode authentication to true for testing
+  const isAuthenticated = true;
   console.log('SCVolunteerCell - isAuthenticated:', isAuthenticated);
 
   return <CCVolunteerCell service={props.service} isAuthenticated={isAuthenticated} />;
